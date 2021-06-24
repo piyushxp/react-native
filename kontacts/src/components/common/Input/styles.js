cacfbd5766badbfd79929ea8d16975b69a87385d
piyushxp <piyushmahapatra001@gmail.com>
2021-06-24 16:31:17 +0530

    Login Component

--- kontacts/src/components/common/Input/styles.js
+++ kontacts/src/components/common/Input/styles.js
@@ -17,6 +17,7 @@ export default StyleSheet.create({
   textInput: {
     flex: 1,
     width: '100%',
+    color: 'black',
   },
 
   error: {
--- kontacts/src/screens/Login/index.js
+++ kontacts/src/screens/Login/index.js
@@ -3,37 +3,11 @@ import {Text} from 'react-native';
 import CustomButton from '../../components/common/CustomButton';
 import Container from '../../components/common/Container';
 import Input from '../../components/common/Input';
+import LoginComponent from '../../components/Login';
 
 const Login = () => {
   const [value, onChangeText] = React.useState('');
-  return (
-    <Container>
-      <Input
-        onChangeText={text => onChangeText(text)}
-        value={value}
-        label="Username"
-        iconPosition="right"
-      />
-
-      <Input
-        onChangeText={text => onChangeText(text)}
-        value={value}
-        label="Password"
-        icon={<Text>HIDE</Text>}
-        iconPosition="right"
-      />
-
-      <CustomButton primary title="Submit" loading={true} disabled={false} />
-      <CustomButton
-        secondary
-        title="Click me"
-        loading={true}
-        disabled={false}
-      />
-      <CustomButton danger title="Submit" loading={true} disabled={false} />
-      <CustomButton disabled title="Submit" loading={true} disabled={false} />
-    </Container>
-  );
+  return <LoginComponent />;
 };
 
 export default Login;
