10dd6c328bf522e74e4173450d95e5391f4f198d
piyushxp <piyushmahapatra001@gmail.com>
2021-06-28 12:19:38 +0530

    projectsetup with assets,signup header done

---
+++ wallet/screens/SignUp.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Modal,
+  FlatList,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native';
+
+import LinearGradient from 'react-native-linear-gradient';
+
+import {COLORS, SIZES, icons, FONTS} from '../constants';
+
+export const SignUp = () => {
+  function renderHeader() {
+    return (
+      <TouchableOpacity
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginTop: SIZES.padding * 6,
+          paddingHorizontal: SIZES.padding * 2,
+        }}
+        onPress={() => console.log('Signup')}>
+        <Image
+          source={icons.back}
+          resizeMode="contain"
+          style={{
+            width: 30,
+            height: 30,
+            tintColor: COLORS.white,
+          }}
+        />
+
+        <Text
+          style={{
+            marginLeft: SIZES.padding * 1.5,
+            color: COLORS.white,
+            ...FONTS.h4,
+          }}>
+          Sign up
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+      style={{flex: 1}}>
+      <LinearGradient colors={[COLORS.lime, COLORS.emerald]} style={{flex: 1}}>
+        <ScrollView>{renderHeader()}</ScrollView>
+      </LinearGradient>
+    </KeyboardAvoidingView>
+  );
+};
