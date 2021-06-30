58649d57887f84ffe27fb18bfdf8ee232ebe90ab
piyushxp <piyushmahapatra001@gmail.com>
2021-06-30 16:16:16 +0530

    project-setup,onboard screen

---
+++ sweethomes/src/context/initialStates/contactsInitialState.js
@@ -0,0 +1,17 @@
+export default {
+  getContacts: {
+    data: {},
+    loading: false,
+    error: null,
+  },
+  createContacts: {
+    data: {},
+    loading: false,
+    error: null,
+  },
+  deleteContacts: {
+    data: {},
+    loading: false,
+    error: null,
+  },
+};
