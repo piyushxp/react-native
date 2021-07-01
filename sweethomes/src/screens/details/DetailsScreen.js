9e98e216e318c5eceea84e51e8642ea0e4ee640e
piyushxp <piyushmahapatra001@gmail.com>
2021-07-01 13:31:04 +0530

    details screen done

--- sweethomes/src/screens/details/DetailsScreen.js
+++ sweethomes/src/screens/details/DetailsScreen.js
@@ -1,29 +1,101 @@
 import React from 'react';
-import {View, Text, ImageBackground, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Dimensions,
+  FlatList,
+} from 'react-native';
 import COLORS from '../../consts/colors';
 import styles from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {SIZES} from '../../consts/theme';
+import houses from '../../consts/houses';
+
 const DetailsScreen = ({navigation, route}) => {
   const house = route.params;
 
+  const InteriorImage = ({image}) => {
+    return <Image source={image} style={styles.interiorImage} />;
+  };
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
-      <View style={styles.backgroundImageContainer}>
-        <ImageBackground style={styles.backgroundImage} source={house.image}>
-          <View style={styles.header}>
-            <View style={styles.headerBtn}>
-              <Icon name="arrow-back-ios" size={15} />
-            </View>
+      <ScrollView>
+        <View style={styles.backgroundImageContainer}>
+          <ImageBackground style={styles.backgroundImage} source={house.image}>
+            <View style={styles.header}>
+              <View style={styles.headerBtn}>
+                <Icon name="arrow-back-ios" size={15} />
+              </View>
 
-            <View style={styles.headerBtn}>
-              <Icon name="favorite" size={15} color={COLORS.red} />
+              <View style={styles.headerBtn}>
+                <Icon name="favorite" size={15} color={COLORS.red} />
+              </View>
+            </View>
+          </ImageBackground>
+          <View style={styles.virtualTag}>
+            <Text style={{color: COLORS.white}}>Virtual Tour</Text>
+          </View>
+        </View>
+
+        <View style={styles.detailsContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{fontSize: SIZES.body3, fontWeight: 'bold'}}>
+              {house.title}
+            </Text>
+
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <View style={styles.ratingTag}>
+                <Text style={{color: COLORS.white}}>5.0</Text>
+              </View>
+              <Text style={{fontSize: 13, marginLeft: 5}}>89 Ratings</Text>
             </View>
           </View>
-        </ImageBackground>
-        <View style={styles.virtualTag}>
-          <Text style={{color: COLORS.white}}>Virtual Tour</Text>
+
+          <Text style={{fontSize: SIZES.body4, color: COLORS.grey}}>
+            {house.location}
+          </Text>
+
+          <View style={styles.cardIcons}>
+            <View style={styles.facility}>
+              <Icon name="hotel" size={18}></Icon>
+              <Text style={styles.facilityText}>3</Text>
+            </View>
+
+            <View style={styles.facility}>
+              <Icon name="bathtub" size={18}></Icon>
+              <Text style={styles.facilityText}>1</Text>
+            </View>
+
+            <View style={styles.facility}>
+              <Icon name="aspect-ratio" size={18}></Icon>
+              <Text style={styles.facilityText}>300 area</Text>
+            </View>
+          </View>
+
+          <Text style={{marginTop: 10, color: COLORS.grey}}>
+            {house.details}
+          </Text>
+
+          <FlatList
+            contentContainerStyle={{marginTop: 20}}
+            keyExtractor={(_, key) => key.toString()}
+            horizontal
+            data={house.interiors}
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            renderItem={({item}) => <InteriorImage image={item} />}
+          />
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- sweethomes/src/screens/details/styles.js
+++ sweethomes/src/screens/details/styles.js
@@ -42,4 +42,41 @@ export default styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  detailsContainer: {
+    flex: 1,
+    paddingHorizontal: SIZES.padding * 2,
+    marginTop: 40,
+  },
+
+  ratingTag: {
+    height: 30,
+    width: 30,
+    backgroundColor: COLORS.blue,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  cardIcons: {
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+
+  facility: {
+    flexDirection: 'row',
+    marginRight: 15,
+  },
+
+  facilityText: {
+    marginLeft: 5,
+    color: COLORS.grey,
+    fontSize: SIZES.body5,
+  },
+
+  interiorImage: {
+    width: SIZES.width / 3 - 20,
+    height: 80,
+    marginRight: 10,
+    borderRadius: 20,
+  },
 });
