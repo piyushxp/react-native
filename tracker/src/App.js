07f17a6fa8860d2e67281f2dd463f4f864e26adc
piyushxp <piyushmahapatra001@gmail.com>
2021-07-11 01:08:55 +0530

    project-setup

---
+++ tracker/src/App.js
@@ -0,0 +1,35 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+
+import {createStackNavigator} from '@react-navigation/stack';
+import {COLORS} from './constants/theme';
+import Home from './screens/Home';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: 'transparent',
+  },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        Screenoptions={{
+          headerShown: false,
+        }}
+        initialRouteName={'Home'}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
